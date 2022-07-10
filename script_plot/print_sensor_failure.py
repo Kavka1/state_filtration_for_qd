0db31ac32f4267b8d1e4e9ad2bce6c1c8f5efbfb
Mac <xukang_0317@163.com>
2022-07-10 18:18:45 +0800

    add new script

---
+++ script_plot/print_sensor_failure.py
@@ -0,0 +1,41 @@
+from typing import List, Dict, Tuple
+import pandas as pd
+import numpy as np
+
+
+
+if __name__ == '__main__':
+    all_seeds = [f'{int(n+1) * 10}' for n in range(8)]
+    all_algs    = [
+        'multi',
+        'dvd',
+        'smerl_ppo',
+        'ensemble',
+    ]
+    all_sensor_failure = [
+        '1',
+        '2',
+        '3',
+        '4'
+    ]
+
+    csv_log = {f'coord {sensor}': {f'{alg}': [] for alg in all_algs} for sensor in all_sensor_failure}
+
+    for sensor in all_sensor_failure:
+        for alg in all_algs:
+            
+            performance_over_all_seeds = []
+            for seed in [f'{(s+1)*10}' for s in range(8)]:
+                csv_path = f'/home/xukang/Project/state_filtration_for_qd/statistic/{alg}/Ant-defective_sensor-coord_{sensor}-{seed}.csv'
+                df = pd.read_csv(csv_path)
+                df_values = df.values
+                performance_over_all_seeds.append(np.max(df_values))
+
+                #csv_log[sensor][alg].append(np.max(df_values))
+            mean, std = np.mean(performance_over_all_seeds), np.std(performance_over_all_seeds)
+
+            print(f'{sensor}-{alg}: mean - {round(mean, 2)} std - {round(mean, 2)}')
+            csv_log[sensor][alg] += [round(mean, 2), round(mean, 2)]
+
+    csv_log = pd.DataFrame(csv_log)
+    csv_log.to_csv('/home/xukang/Project/state_filtration_for_qd/statistic/defective_sensor.csv')
